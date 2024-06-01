02d5e1cecee0ab00522625bd5a2cb31f0fec7dd6
seyam <seyamibrahim88@gmail.com>
2024-06-01 23:42:27 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -73,39 +73,37 @@ function App() {
   return (
     < >
     <Navbar/> 
-       <div className="mx-3 md:container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[40%]">
-        <h1 className='font-bold text-center text-2xl'>iTask - Manage your todos at one place</h1>
-         <div className="addTodo my-5 flex flex-col gap-4">
-          <h2 className='text-xl font-bold'>Add a Todo</h2>
+       <div className="mx-3 md:container md:mx-auto my-3 rounded-xl p-4 bg-violet-100 min-h-[80vh] md:w-[40%]">
+        <h1 className='text-2xl font-bold text-center'>iTask - Manage your todos at one place</h1>
+        <div className="addTodo my-4 flex flex-col gap-3">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
           <div className="flex">
+            <input onChange={handleChange} value={todo} type="text" className='w-full rounded-full px-3 py-1' />
+            <button onClick={handleAdd} disabled= {todo.length < 3} className='bg-violet-900 mx-2 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-2 py-2 text-sm font-bold text-white'>Save</button>
+          </div>
+        </div>
+        <input className='my-3' onChange={toggleFinished} type="checkbox" checked={showFinished} name="" id="show" /> 
+        <label className="mx-2" htmlFor="show">Show Finished</label>
+        <div className='h-[1px] bg-black opacity-20 w-3/4 mx-auto my-2'></div>
+        <div className='text-xl font-bold'>Your Todos</div>
+        <div className="todos w-full">
+          {todos.length === 0 && <div className='m-3'>No Todos to display</div>}
 
-          <input  onChange={handleChange} value={todo} type="text" className='w-full rounded-full px-5 py-1' />
-          <button onClick={handleAdd} disabled={todo.length<=3} className='bg-violet-800 mx-2 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-4 py-2 text-sm font-bold text-white'>Save</button>
-          </div>
-         </div>
-         <input className='my-4' id='show' onChange={toggleFinished} type="checkbox" checked={showFinished} /> 
-         <label className='mx-2' htmlFor="show">Show Finished</label> 
-         <div className='h-[1px] bg-black opacity-15 w-[90%] mx-auto my-2'></div>
-         <h2 className='text-xl font-bold'>Your Todos</h2>
-         <div className="todos">
-          {todos.length ===0 && <div className='m-5'>No Todos to display</div> }
           {todos.map(item=>{
- 
-          return (showFinished || !item.isCompleted) && <div key={item.id} className={"todo flex my-3 justify-between"}>
-            <div className='flex  gap-5'> 
-            <input name={item.id} onChange={handleCheckbox} type="checkbox" checked={item.isCompleted} id="" />
-            <div className={item.isCompleted?"line-through":""}>
-              <p className='text-wrap'>{item.todo}</p>
+            return (showFinished || !item.isCompleted) && <div key={item.id} className={'todo flex justify-between  my-2 '}>
+              <div className='flex justify-start gap-4'>
+              <input type="checkbox" onChange={handleCheckbox} name={item.id} checked={item.isCompleted} id="" />
+              <div className={item.isCompleted?"line-through":""}>
+                <p className='text-wrap'>{item.todo}</p>
+              </div>
+              </div>
+              <div className="buttons flex h-full">
+                <button onClick={(e)=>{handleEdit(e,item.id)}} className='bg-violet-800 mx-1 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-2 py-2 text-sm font-bold text-white'><FaEdit /></button>
+                <button onClick={(e)=>{handleDelete(e,item.id)}} className='bg-violet-800 mx-1 rounded-full hover:bg-violet-950 disabled:bg-violet-500 p-2 py-2 text-sm font-bold text-white'><AiFillDelete/></button>
+              </div>
             </div>
-            </div>
-            <div className="buttons flex h-full">
-              <button onClick={(e)=>handleEdit(e, item.id)} className='bg-violet-800 hover:bg-violet-950 p-2 py-2 text-sm font-bold text-white rounded-md mx-1'><FaEdit /></button>
-              <button onClick={(e)=>{handleDelete(e, item.id)}} className='bg-violet-800 hover:bg-violet-950 p-2 py-2 text-sm font-bold text-white rounded-md mx-1'><AiFillDelete /></button>
-            </div> 
-          </div>
           })}
-         </div>
-        
+        </div>
        </div>
     </>
   )
